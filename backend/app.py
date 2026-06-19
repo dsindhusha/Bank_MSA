8d986f0050d8decf97b1da5547850a2e6ccf9a2d
dsindhusha <sindhushadevarakonda@gmail.com>
2026-06-19 21:01:03 +0530

    Fixed CORS

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,10 @@ app = FastAPI(
 # Allow React frontend to call FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://bank-35ealy2ms-dsindhushas-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
